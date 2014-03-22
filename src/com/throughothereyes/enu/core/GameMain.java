5f28d7dfa1f95f796119b3e61a1917a41e9900dc
Jonathan Platzer <jonathan1996@gmx.at>
2014-03-22 13:28:06 +0000

    Created packages and started with 4:3 aspect ratio fullscreen gamepanel

---
+++ src/com/throughothereyes/enu/core/GameMain.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.throughothereyes.enu.core;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.DisplayMode;
+import java.awt.Graphics2D;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import javax.swing.JFrame;
+
+/**
+ * @author Jonathan Platzer
+ * @date 22.03.2014
+ */
+public class GameMain extends JFrame {
+
+    private final GraphicsDevice device;
+    private DisplayMode displayMode;
+    private GamePanel gamePanel;
+
+    public GameMain(GraphicsDevice device) {
+        this.device = device;
+    }
+
+    public void initialize() {
+        if (device.isFullScreenSupported()) {
+            setUndecorated(true);
+            setLayout(null);
+            getContentPane().setBackground(Color.BLACK);
+            displayMode = device.getDisplayMode();
+            device.setFullScreenWindow(this);
+            int height = displayMode.getHeight();
+            int width = (int)Math.round(1.3333333333333*displayMode.getHeight());
+            float zoomRatio = height/480f;
+            gamePanel = new GamePanel(width, height, zoomRatio);
+            gamePanel.setBounds((displayMode.getWidth() - width) / 2, 0, width, height);
+            add(gamePanel);
+            validate();
+            setVisible(true);
+        } else {
+            setResizable(false);
+        }
+    }
+
+    public void start() {
+
+    }
+
+    public void shutdown() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void draw(Graphics2D g2d) {
+
+    }
+
+    public static void main(String[] args) {
+        GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice defaultDevice = env.getDefaultScreenDevice();
+        GameMain game = new GameMain(defaultDevice);
+        game.initialize();
+    }
+}
---
+++ src/com/throughothereyes/enu/core/GamePanel.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.throughothereyes.enu.core;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JPanel;
+
+/**
+ * @author Jonathan Platzer
+ * @date 22.03.2014
+ */
+public class GamePanel extends JPanel {
+
+    private float ratio;
+    
+    public GamePanel(int width, int height, float zoomRatio)
+    {
+        this.setSize(width, height);
+        this.ratio = zoomRatio;
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        RenderingHints rh
+                       = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
+                                            RenderingHints.VALUE_ANTIALIAS_ON);
+
+        rh.put(RenderingHints.KEY_RENDERING,
+               RenderingHints.VALUE_RENDER_QUALITY);
+
+        g2.setRenderingHints(rh);
+        
+        g2.setStroke(new BasicStroke(1));
+        g2.setColor(Color.gray);
+        g2.setBackground(Color.BLACK);
+        System.out.println(this.getSize());
+        g2.fillRect((int) Math.round(0*ratio), (int) Math.round(0*ratio), (int) Math.round(320*ratio), (int) Math.round(240*ratio));
+        
+        g2.fillRect((int) Math.round(320*ratio), (int) Math.round(240*ratio), (int) Math.round(320*ratio), (int) Math.round(240*ratio));
+
+//        Dimension size = getSize();
+//        double w = size.getWidth();
+//        double h = size.getHeight();
+//
+//        Ellipse2D e = new Ellipse2D.Double(0, 0, 80, 130);
+//        g2.setStroke(new BasicStroke(1));
+//        g2.setColor(Color.gray);
+//
+//        for (double deg = 0; deg < 360; deg += 5) {
+//            AffineTransform at
+//                            = AffineTransform.getTranslateInstance(w / 2, h / 2);
+//            at.rotate(Math.toRadians(deg));
+//            g2.draw(at.createTransformedShape(e));
+//        }
+    }
+}
