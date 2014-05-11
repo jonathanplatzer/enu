5f8c9a5f3c4224f12c6a5091d8c71406619d7366
Jonathan Platzer <jonathan1996@gmx.at>
2014-05-11 13:27:58 +0000

    Load all Countries into the Game
    Load all Countries into the Game and display them. At the moment 
    it simply loads all Objects from res/map into the countries ArrayList

--- src/com/through_other_eyes/enu/obj/Map.java
+++ src/com/through_other_eyes/enu/obj/Map.java
@@ -14,13 +14,14 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.through_other_eyes.enu.obj;
 
 import com.through_other_eyes.enu.core.GameCore;
 import com.through_other_eyes.enu.obj.base.GameComponent;
 import com.through_other_eyes.enu.obj.base.UIElement;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -33,30 +34,43 @@ import javax.imageio.ImageIO;
  */
 public class Map extends GameComponent {
 
-    private ArrayList<GameComponent> mapLayer = new ArrayList<>();
+    private ArrayList<Country> countries = new ArrayList<>();
     private ArrayList<UIElement> uiElements = new ArrayList<>();
-    private BufferedImage mapImage;
-    
+    private Image background;
+
     public Map(BufferedImage mapImage) throws IOException {
+        loadMap();
         InstitutionMenu instMenu = new InstitutionMenu(ImageIO.read(new File("res" + File.separator + "institutionmenu.png")), GameCore.Align.CENTER, 0, 0);
         uiElements.add(instMenu);
-        this.mapImage = mapImage;
     }
-    
+
+    public void loadMap() throws IOException {
+        background = Toolkit.getDefaultToolkit().createImage("res" + File.separator + "map" + File.separator + "background.gif");
+        for (final File file : new File("res" + File.separator + "map").listFiles()) {
+            if(file.isFile() && !file.getName().equals("background.gif")) {
+                countries.add(new Country(file.getName(), ImageIO.read(file)));
+            }
+        }
+    }
+
     @Override
     public void update() {
 //        x = x!=1 ? 1 : -1;
 //        y = x!=1 ? 1 : -1;
     }
 
-    int x,y;
-    
+    int x, y;
+
     @Override
     public void drawObject(Graphics2D g2) {
-        g2.drawImage(mapImage, x, y, null);
+        g2.drawImage(background, 0, 0, null);
+        for (Country country : countries) {
+            if(country.isVisible()) {
+                country.drawObject(g2);
+            }
+        }
         for (UIElement uilement : uiElements) {
-            if(uilement.isVisible())
-            {
+            if (uilement.isVisible()) {
                 uilement.drawObject(g2);
             }
         }
@@ -67,13 +81,12 @@ public class Map extends GameComponent {
     }
 
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
     public InstitutionMenu getInstMenu() {
         for (UIElement uilement : uiElements) {
-            if(uilement instanceof InstitutionMenu)
-            {
+            if (uilement instanceof InstitutionMenu) {
                 return (InstitutionMenu) uilement;
             }
         }
